1c4b29a54eaff2bc577f8bade0731c7b726ab19d
Александр <alteleg@yandex.ru>
2024-03-15 03:22:48 +0300

    Fixes

--- storage_server/storage_server/urls.py
+++ storage_server/storage_server/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('storage_server.accounts.urls')),
-    path('/files/', include('storage_server.files.urls')),
+    path('files/', include('storage_server.files.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
