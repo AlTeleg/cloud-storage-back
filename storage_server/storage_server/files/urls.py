c62da3ddabdec9714d1550edef48c17897fe29d0
Александр <alteleg@yandex.ru>
2024-03-25 06:06:33 +0300

    Fixes

--- storage_server/storage_server/files/urls.py
+++ storage_server/storage_server/files/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('<int:file_id>/comment/', views.CommentView.as_view(), name='file-comment'),
     path('<int:file_id>/download/', views.DownloadView.as_view(), name='file-download'),
     path('<int:file_id>/share/', views.UserShareView.as_view(), name='file-share'),
-    re_path(r'^files/(?P<special_link>.+)/$', views.DownloadSpecialView.as_view(), name='download-special')
+    re_path(r'^files/(?P<file_id>\d+)/(?P<token>[a-fA-F0-9]{32})/$', views.DownloadSpecialView.as_view(),
+            name='download-special')
 ]
